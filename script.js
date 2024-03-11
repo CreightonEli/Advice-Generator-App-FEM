f1ef58aeeb7de87b28d557b995da67b51ae49dc4
Elijah Creighton <89100976+CreightonEli@users.noreply.github.com>
2024-03-11 00:22:12 -0400

    got to work on scripting in the api
    - got api working and connected to index.html, but can't seem to get it to update upon button press or page refresh.
        - maybe a caching issue. Gonna have to look into further later.
    - minor html and css changes

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 // https://www.youtube.com/watch?v=2AfzKmgqWUE
+const resID = document.querySelector('#adviceID')
+const resAdv = document.querySelector('#results')
+const resBtn = document.querySelector('#getData')
+
+
+
+resBtn.addEventListener('click', () => {
+    getAdvice();
+})
+
+window.onload = () => {
+    getAdvice();
+}
+
+function getAdvice(){
+
+    fetch("https://api.adviceslip.com/advice")
+        .then(response => {
+            return response.json();
+        })
+        .then(adviceData => {
+            console.log(adviceData);
+            const adviceObj = adviceData.slip;
+            resID.innerHTML = adviceObj.id;
+            resAdv.innerHTML = adviceObj.advice;
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+}
